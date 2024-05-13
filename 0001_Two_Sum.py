c08b2de496915081bcec325d466998b0b96fb512
German Mercader <121909886+GermanMer@users.noreply.github.com>
2024-05-13 14:17:31 -0300

    Add files via upload

--- 0001_Two_Sum.py
+++ 0001_Two_Sum.py
@@ -15,23 +15,24 @@
 #Input: nums = [3,3], target = 6
 #Output: [0,1]
 
-def two_sum(nums, target):
-    num_dict = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in num_dict:
-            return [num_dict[complement], i]
-        num_dict[num] = i
+class Solution:
+    def twoSum(self, nums, target):
+        num_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
 
 # Ejemplos de uso:
 nums = [2, 7, 11, 15]
 target = 9
-print(two_sum(nums, target))  # Output: [0, 1]
+print(Solution().twoSum(nums, target))  # Output: [0, 1]
 
 nums = [3,2,4]
 target = 6
-print(two_sum(nums, target))  # Output: [1, 2]
+print(Solution().twoSum(nums, target))  # Output: [1, 2]
 
 nums = [3,3]
 target = 6
-print(two_sum(nums, target))  # Output: [0, 1]
+print(Solution().twoSum(nums, target))  # Output: [0, 1]
