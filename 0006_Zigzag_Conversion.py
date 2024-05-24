bab2b33629c70668fa5cbe1bd23da7981b9d6325
German Mercader <121909886+GermanMer@users.noreply.github.com>
2024-05-24 13:06:47 -0300

    Add files via upload

---
+++ 0006_Zigzag_Conversion.py
@@ -0,0 +1,92 @@
+#The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+#P   A   H   N
+#A P L S I I G
+#Y   I   R
+
+#And then read line by line: "PAHNAPLSIIGYIR"
+
+#Write the code that will take a string and make this conversion given a number of rows:
+#string convert(string s, int numRows)
+
+#Example 1:
+#Input: s = "PAYPALISHIRING", numRows = 3
+#Output: "PAHNAPLSIIGYIR"
+#Explanation:
+#P   A   H   N
+#A P L S I I G
+#Y   I   R
+
+#Example 2:
+#Input: s = "PAYPALISHIRING", numRows = 4
+#Output: "PINALSIGYAHRPI"
+#Explanation:
+#P     I    N
+#A   L S  I G
+#Y A   H R
+#P     I
+
+#Example 3:
+#Input: s = "A", numRows = 1
+#Output: "A"
+
+#Constraints:
+#1 <= s.length <= 1000
+#s consists of English letters (lower-case and upper-case), ',' and '.'.
+#1 <= numRows <= 1000
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if len(s) < 2:
+            return s
+        else:
+            contadorUp = 1
+            contadorDw = numRows - 1
+            repartidor = []
+            for i in s:
+                if contadorUp <= numRows:
+                    repartidor.append(contadorUp)
+                    contadorUp = contadorUp + 1
+                    contadorDw = numRows - 1
+                elif contadorDw < numRows and contadorDw > 1:
+                    repartidor.append(contadorDw)
+                    if contadorDw == 2:
+                        contadorUp = 1
+                    else:
+                        contadorDw = contadorDw - 1
+
+            resultado = ""
+            current_row = 1
+            for y in range(numRows):
+                for x in repartidor:
+                    try:
+                        index = repartidor.index(current_row)
+                        resultado = resultado + s[index]
+                        repartidor[index] = 0
+                    except: continue
+                current_row = current_row + 1
+            return resultado
+
+#Example 1:
+s = "PAYPALISHIRING"
+numRows = 3
+print(Solution().convert(s, numRows))
+
+#Example 2:
+s = "PAYPALISHIRING"
+numRows = 4
+print(Solution().convert(s, numRows))
+
+#Example 3:
+s = "A"
+numRows = 1
+print(Solution().convert(s, numRows))
+
+
+
+
+#0 1 2 3  4 5 6 7 8 9  10 11 12 13
+#P A Y P  A L I S H I  R  I  N  G
+#1 2 3 2  1 2 3 2 1 2  3  2  1  2
+#P A H N  A P L S I I  G  Y  I  R
+#0 4 8 12 1 3 5 7 9 11 13 2  6  10
